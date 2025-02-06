adfb47a1e819db0206b7f5abd55b46358404f095
Geoffrey Choy <geoffchoy@outlook.com>
2025-02-06 16:21:14 +0000

    Delete secret immediately

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,6 +1,7 @@
 resource "aws_secretsmanager_secret" "sm-redis-user-secret" {
   name = "templates/${var.lambda_name}/redis-user-secret"
   description = "Redis user secret for the .NET Web API Lambda"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "sm-redis-user-secret" {
